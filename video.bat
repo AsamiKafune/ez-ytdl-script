1a676dbaf9d9878f6e34f1521ab915a838f59c55
Kafune CH <103914713+AsamiKafune@users.noreply.github.com>
2023-08-14 20:06:16 +0700

    fixed: update for 1080p

--- video.bat
+++ video.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p link="linkyt: "
-yt-dlp %link% -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
+yt-dlp %link% -f "bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
 
 pause
